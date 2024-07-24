0850fd3c859b06a8e1976a21c42b6a3bfaff5b45
A Chaithanya Kumar <100756558+iamCodeConqueror@users.noreply.github.com>
2024-07-24 12:02:20 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://chaithanyakumar.netlify.app'
 
 ];
 
