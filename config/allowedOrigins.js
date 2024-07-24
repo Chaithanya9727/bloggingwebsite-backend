618d3d212577dfde7f56201f79524313c79663d5
A Chaithanya Kumar <100756558+iamCodeConqueror@users.noreply.github.com>
2024-07-24 17:47:51 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,10 @@ const allowedOrigins = [
     'http://localhost:4200',
     'http://localhost:5174',
     'https://chaithanyakumar.netlify.app',
-    'https://dancing-moonbeam-42d2e8.netlify.app'
+    'https://dancing-moonbeam-42d2e8.netlify.app',
+    'https://bloggingwebsite-frontend.vercel.app',
+    'https://bloggingwebsite-frontend-git-main-chaithanyakumars-projects.vercel.app',
+    'https://bloggingwebsite-frontend-chaithanyakumars-projects.vercel.app'
 
 ];
 
