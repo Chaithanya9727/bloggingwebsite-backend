e8fef9ab4c88e460f4f67094d0649fd09fb7920c
A Chaithanya Kumar <100756558+iamCodeConqueror@users.noreply.github.com>
2024-07-24 13:35:08 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
     'http://localhost:5174',
-    'https://chaithanyakumar.netlify.app'
+    'https://chaithanyakumar.netlify.app',
+    'https://dancing-moonbeam-42d2e8.netlify.app'
 
 ];
 
