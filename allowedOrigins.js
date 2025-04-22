00235da0631c116375d82ec9062e81d98681bb58
A Chaithanya Kumar <100756558+Chaithanya9727@users.noreply.github.com>
2025-04-22 20:34:44 +0530

    Add files via upload

---
+++ allowedOrigins.js
@@ -0,0 +1,15 @@
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:4200',
+    'http://localhost:5174',
+    'https://chaithanyakumar.netlify.app',
+    'https://sampathblog.netlify.app',
+    'https://dancing-moonbeam-42d2e8.netlify.app',
+    'https://bloggingwebsite-frontend.vercel.app',
+    'https://bloggingwebsite-frontend-git-main-chaithanyakumars-projects.vercel.app',
+    'https://bloggingwebsite-frontend-chaithanyakumars-projects.vercel.app'
+
+];
+
+module.exports = allowedOrigins;
